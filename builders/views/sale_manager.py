5c4e0b0521a79cbe578d9d064f118e81fde7b867
Firuz96-04 <franksoft96@gmail.com>
2023-05-10 17:34:34 +0500

    desktop filters

--- builders/views/sale_manager.py
+++ builders/views/sale_manager.py
@@ -4,18 +4,28 @@ from builders.serializers import (
     ClientNoticeApartmentSerializer,
 )
 from builders.permissions import OnlySaleManagerPermission
-from builders.models import Client
+from builders.models import Client, SaleManager
+from django_filters import rest_framework as filters
+from builders.filters import ClientFilter
 
 
 class ClientNoticeApartmentApiView(mixins.ListModelMixin,
                                    mixins.CreateModelMixin,
                                    generics.GenericAPIView):
-    queryset = Client.objects.all()
     serializer_class = ClientNoticeApartmentSerializer
     permission_classes = (OnlySaleManagerPermission,)
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_class = ClientFilter
+
+    def get_queryset(self):
+        sale_manager_id = self.request.user.id
+        sale_manager = SaleManager.objects.get(user_id=sale_manager_id)
+        manager_id = sale_manager.manager_id
+        return Client.objects.filter(sale_manager__manager_id=manager_id).select_related('sale_manager', 'social_media')
 
     def get(self, request, *args, **kwargs):
-        serial = ClientNoticeApartmentSerializer(self.get_queryset(), many=True)
+        query = self.filter_queryset(self.get_queryset())
+        serial = ClientNoticeApartmentSerializer(query, many=True)
         return Response({'data': serial.data})
 
     def post(self, request, *args, **kwargs):
